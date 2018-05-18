3cba38fc416f7a0523cf7f08d8ec9b08d9e88e8d
unknown <phuwaditever@gmail.com>
2018-05-18 15:48:13 +0700

    third_commit

--- src/views/Pages/Login/Login.js
+++ src/views/Pages/Login/Login.js
@@ -43,8 +43,7 @@ class Login extends Component {
                   <CardBody className="text-center">
                     <div>
                       <h2>Sign up</h2>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua.</p>
+                      <p>Welcome to GPS Tracking.</p>
                       <Button color="primary" className="mt-3" active>Register Now!</Button>
                     </div>
                   </CardBody>
